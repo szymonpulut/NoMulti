15f42e62f2675428dfa3dc75684c3b8cbaced01d
Szymon Pulut <szymoneks12@o2.pl>
2012-05-20 13:39:28 +0200

    Now it is stable build ;)

--- src/com/github/Invicter/NoMulti.java
+++ src/com/github/Invicter/NoMulti.java
@@ -11,7 +11,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.Listener;
 
@@ -24,10 +24,9 @@ public class NoMulti extends JavaPlugin implements Listener {
 	public void reloadCustomConfig() {
 	    if (customConfigFile == null) {
 	    customConfigFile = new File(getDataFolder(), "players.yml");
-	    }	
+	    }
 	    customConfig = YamlConfiguration.loadConfiguration(customConfigFile);
-	 
-
+	    
 	    InputStream defConfigStream = getResource("players.yml");
 	    if (defConfigStream != null) {
 	        YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
@@ -58,10 +57,10 @@ public class NoMulti extends JavaPlugin implements Listener {
 	{
 		getServer().getPluginManager().registerEvents(this, this);
 		Logger.getLogger(JavaPlugin.class.getName()).log(Level.INFO, "[NoMulti] Enabling plugin");
-		reloadConfig();
-		saveConfig();
-		reloadCustomConfig();
-		saveCustomConfig();
+		this.getConfig().options().copyDefaults(true);
+        this.saveConfig();
+        this.getCustomConfig().options().copyDefaults(true);
+        this.saveCustomConfig();
 		Logger.getLogger(JavaPlugin.class.getName()).log(Level.INFO, "[NoMulti] Plugin enabled");
 	}
 	
@@ -69,38 +68,41 @@ public class NoMulti extends JavaPlugin implements Listener {
 	public void onDisable()
 	{
 		Logger.getLogger(JavaPlugin.class.getName()).log(Level.INFO, "[NoMulti] Disabling plugin");
-		saveConfig();
-		saveCustomConfig();
+		this.saveConfig();
+		this.saveCustomConfig();
 		Logger.getLogger(JavaPlugin.class.getName()).log(Level.INFO, "[NoMulti] Plugin disabled");
 	}
 
 	@EventHandler
-	public boolean playerJoinEvent(PlayerJoinEvent event){
+	public boolean onPlayerLogin(PlayerLoginEvent event){
 
 		Player player = event.getPlayer();
-		String adress = player.getAddress().getHostName();
-		adress.replace("/", "");
-
+		String adress = event.getAddress().getHostAddress();
 		String playername = player.getName();
-		
+        
+		this.saveCustomConfig();
+        this.reloadCustomConfig();
+        
 		if(!player.hasPermission("nomulti.exempt"))
 		{
-			if (this.getCustomConfig().getString(adress) == null
-					|| this.getCustomConfig().getString(adress) == "NULL"
-					|| this.getCustomConfig().getString(adress) == "null"
-					|| this.getCustomConfig().getString(adress) == "") {
-				this.getCustomConfig().set(adress, playername);
-			} else if (this.getCustomConfig().getString(adress) != null
-					&& this.getCustomConfig().getString(adress) != "NULL"
-					&& this.getCustomConfig().getString(adress) != "null"
-					&& this.getCustomConfig().getString(adress) != "") {
-	
-				if (playername != this.getCustomConfig().getString(adress)) {
-					player.kickPlayer(getConfig().getString("kick-message"));
-					Logger.getLogger(JavaPlugin.class.getName()).log(Level.INFO, "Disconnecting "+playername+" [/"+adress+"]: "+getConfig().getString("kick-message"));
+				if (playername != this.getCustomConfig().getString(adress)) 
+				{
+					event.setKickMessage(getConfig().getString("kick-message"));
+			    	event.setResult(PlayerLoginEvent.Result.KICK_OTHER);
 				}
-			}
+				else if (playername == this.getCustomConfig().getString(adress))
+				{
+					//Nothing
+				}
+				else 
+				{
+					this.getCustomConfig().set(adress, playername);
+				}
+				
+	        this.saveCustomConfig();
+	        this.reloadCustomConfig();
+	        
 		}
-		return true;
+	return true;
 	}
 }
